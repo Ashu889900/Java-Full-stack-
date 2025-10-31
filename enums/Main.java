287c0b9cb34be4fa6794bbbb6562c3ea4d9127c3
Ashu889900 <akje269@gmail.com>
2025-10-31 06:01:34 +0000

    added enum example !

---
+++ enums/Main.java
@@ -0,0 +1,25 @@
+enum Status {
+    Running, Failed, Pending, Success; 
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Status s = Status.Running;
+
+        switch (s) {
+            case Running:
+                System.out.println("All good");
+                break;
+            case Failed:
+                System.out.println("Status Failed");
+                break;
+            case Pending:
+                System.out.println("On process");
+                break;
+            case Success:
+                System.out.println("Done");
+                break;
+        }
+    }
+}
+ 
